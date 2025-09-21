c2857a251ef4fb79e302ec61819dfa542e933c19
Asoda123 <shuliak.ivan39@gmail.com>
2025-09-21 13:19:48 +0300

    Easy register system (possible SQL injections). Almost no CSS.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from http.client import responses
-from locale import currency
-
-import json
-
-import flask
+# from http.client import responses
+# from locale import currency
+#
+# import json
+#
+# # import flask
 from flask import Flask
 from flask import url_for, redirect
 from flask import render_template, request
-import requests
-import json
+# import requests
+# import json
 from sqlalchemy import Table, Column ,create_engine , String, ForeignKey
-from typing import List, Optional
+# from typing import List, Optional
 from sqlalchemy.orm import DeclarativeBase , sessionmaker, Mapped, mapped_column, relationship
 
 app = Flask(__name__)
@@ -32,8 +32,15 @@ class Expenses(Base):
     purpose: Mapped[str] = mapped_column(String(50))
     date : Mapped[str] = mapped_column(String(50))
 
+class Users(Base):
+    __tablename__ = 'users'
+    id : Mapped[int] = mapped_column(primary_key=True)
+    login : Mapped[str] = mapped_column(String(20))
+    password : Mapped[str] = mapped_column(String(20))
+
 # base = Base()
 # base.create_db()
+
 
 @app.route('/exp', methods=['GET','POST'])
 def get_info():
@@ -58,6 +65,35 @@ def all_exps():
         data = view.query(Expenses).all()
         return render_template('all_expenses.html', data=data)
 
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    if request.method == "GET":
+        return render_template("reg.html")
+    else:
+        login = request.form['login']
+        password = request.form['password']
+
+        with Session() as sess:
+            new_us = Users(login=login, password=password)
+            sess.add(new_us)
+            sess.commit()
+
+        return redirect(url_for('get_info'))
+
+@app.route('/sign_in', methods=['GET','POST'])
+def sign_in():
+    if request.method == "GET":
+        return render_template("sign_in.html")
+    else:
+        login = request.form['login']
+        password = request.form['password']
+
+        with Session() as sess:
+            new_us = Users(login=login, password=password)
+            sess.add(new_us)
+            sess.commit()
+
+        return redirect(url_for('get_info'))
 
 
 if __name__ == '__main__':
